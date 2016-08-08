1385eeefd42564bed32e39aa38fe37bc4d4f244b
Melanie Berrut <melanieberrut.dev@gmail.com>
2016-08-08 21:16:30 +0100

    Adding a classified and showing notification of success

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -5,7 +5,7 @@
 		.module("ngClassifieds")
 
 		// Injecting $scope and the new factory
-		.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav){ 
+		.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){ 
 			
 		
 				classifiedsFactory.getClassifieds()
@@ -13,6 +13,14 @@
 				.then(function(classifieds){
 					$scope.classifieds = classifieds.data;
 				});
+
+				// Fake out user logged in contact details
+				var contact = {
+					name: "Melanie B",
+					phone: "0707070707070",
+					email: "test@test.com"
+				}
+
 
 				$scope.openSidebar = function() {
 					// open the sidebar
@@ -25,6 +33,34 @@
 
 					// using componenet ID
 					$mdSidenav("left").close();
+				}
+
+				// parsing the classified object to the array
+				$scope.saveClassified = function(classified){
+					// verify that at least one property is defined before sending
+					// the object
+					// to do: add validation on the form
+					if (classified) {
+						// attaching the contact details to object
+						classified.contact = contact;
+						$scope.classifieds.push(classified);
+
+						// Empty the submitted data to allow the user to enter new
+						$scope.classified = {};
+
+						// close sidebar on save
+						$scope.closeSidebar();
+
+						// notfication to show
+						$mdToast.show(
+							$mdToast.simple()
+								.content("Classfied Save!")
+								.position("top, right")
+								.hideDelay("3000")
+						);
+					}
+
+
 				}
 
 			
