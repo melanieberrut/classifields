2d247c8d073adf260eb09c27416205687a852c56
Melanie Berrut <melanieberrut.dev@gmail.com>
2016-08-20 14:43:04 +0100

    removing logs

--- components/classifieds/new/classifieds.new.ctr.js
+++ components/classifieds/new/classifieds.new.ctr.js
@@ -6,14 +6,12 @@
 		.controller("newClassifiedsCtrl", function($mdSidenav, $mdDialog, $timeout, classifiedsFactory){
 			
 			var vm = this;
-			console.log('inside');
 			
 			// note: when going to the new state /new, the side nav is not opening
 			// due the event loop
 			// fix: timeout
 
 			$timeout(function(){
-				console.log('inside tm');
 				$mdSidenav("left").open();
 			});
 
